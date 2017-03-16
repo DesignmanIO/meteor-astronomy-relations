7b3bebaf9845e720e42daec067081aea09461aeb
Julian Kingman <julian@designman.io>
2017-03-16 08:16:49 -0400

    first commit

---
+++ package.js
@@ -0,0 +1,18 @@
+Package.describe({
+  name: 'buishi:astronomy-relations-behavior',
+  version: '2.0.0',
+  summary: 'Timestamp behavior for Meteor Astronomy v2',
+  git: 'https://github.com/DesignmanIO/meteor-astronomy-2-relations.git'
+});
+
+Package.onUse(function(api) {
+  api.versionsFrom('1.3');
+
+  api.use([
+    'ecmascript',
+    'es5-shim',
+    'jagi:astronomy@2.2.0'
+  ], ['client', 'server']);
+
+  api.mainModule('lib/main.js', ['client', 'server']);
+});
